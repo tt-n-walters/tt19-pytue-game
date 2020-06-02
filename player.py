33382b14c80a8a9662668a423a869da09c4cac63
nw <nico.walters@techtalents.es>
2020-06-02 19:00:37 +0200

    Selecting player texture

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ from arcade import Sprite, load_texture
 
 class Player(Sprite):
     def __init__(self, center_x, center_y):
-        super().__init__()
-        self.texture = load_texture("assets/player/down.png")
+        super().__init__(scale=2)
+        self.texture = load_texture("assets/player/down.png", width=42, height=50)
         self.center_x = center_x
         self.center_y = center_y
