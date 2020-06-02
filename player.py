73acceb9aa564e2ed3d99a88b63a36e64b0d1efe
nw <nico.walters@techtalents.es>
2020-06-02 18:52:50 +0200

    Defined initial player texture

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
-from arcade import Sprite
+from arcade import Sprite, load_texture
 
 
 class Player(Sprite):
     def __init__(self, center_x, center_y):
         super().__init__()
+        self.texture = load_texture("assets/player/down.png")
