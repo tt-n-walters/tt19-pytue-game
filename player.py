313f28dafef69645e38de2c620045b683c3c099a
nw <nico.walters@techtalents.es>
2020-06-16 18:08:29 +0200

    More image loading

--- player.py
+++ player.py
@@ -15,6 +15,15 @@ right = load_textures("assets/player/right.png", [[0, 0, 50, 50],
                                                   [50, 0, 50, 50],
                                                   [100, 0, 50, 50]])
 
+stationary_up = up[0]
+stationary_down = down[0]
+stationary_left = left[0]
+stationary_right = right[0]
+
+moving_up = up
+moving_down = down
+moving_left = left
+moving_right = right
 
 class Player(Character):
     def __init__(self, center_x, center_y):
