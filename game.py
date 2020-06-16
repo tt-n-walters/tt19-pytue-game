734fd1dcc562b210e0f7d73bf8cec0a1f9db13cd
nw <nico.walters@techtalents.es>
2020-06-16 18:47:37 +0200

    Added comments

--- game.py
+++ game.py
@@ -7,10 +7,12 @@ class Game(arcade.Window):
     def __init__(self):
         super().__init__(title="Game", width=15 * 64, height=9 * 64, fullscreen=False)
         
+        # Load background image
         self.background_sprite = arcade.Sprite("assets/background.png", center_x=self.width / 2, center_y=self.height / 2)
         self.static_sprites = arcade.SpriteList(is_static=True)
         self.static_sprites.append(self.background_sprite)
 
+        # Create game objects
         self.test_gun = Gun(300, 200)
         self.bullets = []
         self.player = Player(50, 50)
@@ -23,14 +25,17 @@ class Game(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+        # Draw backgroud and all non-moving sprites
         self.static_sprites.draw()
 
+        # Draw game objects
+        self.player.draw()
         self.test_gun.draw()
         for bullet in self.bullets:
             bullet.draw()
-        self.player.draw()
 
     def on_update(self, deltatime):
+        # Update game objects
         for bullet in self.bullets:
             bullet.update()
         self.test_gun.update(deltatime)
